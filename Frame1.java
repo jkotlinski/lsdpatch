b0b5bc20ffc22eaacca3f2498582ca359484bfd7
Johan Kotlinski <kotlinski@gmail.com>
2011-06-27 23:48:25 +0200

    v.16

--- Frame1.java
+++ Frame1.java
@@ -27,7 +27,7 @@ import javax.swing.border.*;
 import java.beans.*;
 
 public class Frame1 extends JFrame {
-    String versionString = "LSD-Patcher v.15";
+    String versionString = "LSD-Patcher v.16";
     JPanel contentPane;
     JPanel jPanel1 = new JPanel();
     TitledBorder titledBorder1;
