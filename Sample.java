5d4c722bded5339676f5e3cca5a7fe0954afd6e3
Johan Kotlinski <kotlinski@gmail.com>
2011-06-29 19:19:26 +0200

    Only change from signed to unsigned, don't add anything extra...

--- Sample.java
+++ Sample.java
@@ -297,7 +297,7 @@ public class Sample {
 
             byte[] unsigned = new byte[buf.length];
             for (int it = 0; it < buf.length; ++it) {
-                unsigned[it] = (byte)((int)buf[it] + 0x88);
+                unsigned[it] = (byte)((int)buf[it] + 0x80);
             }
             wavFile.write(unsigned);
         } catch (Exception e) {
