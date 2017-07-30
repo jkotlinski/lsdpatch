77330959a4245d5e843754286cb1bb3136d9afd2
Eiyeron Fulmincendii <eiyeron@gmail.com>
2017-07-30 21:13:45 +0200

    Forgot to update the build tool

--- make-jar.sh
+++ make-jar.sh
@@ -2,4 +2,4 @@ set -e
 mkdir -p classes
 javac -Xbootclasspath:c:/Program\ Files/Java/jre6/lib/rt.jar -source 1.6 -target 1.6 -Xlint:deprecation -d classes *.java
 cd classes
-jar cvfm ../LSDPatcher.jar ../META-INF/MANIFEST.MF *.class ../*.bmp
+jar cvfm ../LSDPatcher.jar ../META-INF/MANIFEST.MF *.class ../*.bmp ../*.png
