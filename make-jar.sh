b777593f4e766ba4349cbb7e043b1ebd2f660364
Eiyeron Fulmincendii <eiyeron@gmail.com>
2018-09-12 20:26:46 +0200

    Starting to move classes around for a better project structure

--- make-jar.sh
+++
@@ -1,5 +0,0 @@
-set -e
-mkdir -p classes
-javac -Xbootclasspath:c:/Program\ Files/Java/jre6/lib/rt.jar -source 1.6 -target 1.6 -Xlint:deprecation -d classes *.java fontEditor/*.java structures/*.java utils/*.java
-cd classes
-jar cvfm ../LSDPatcher.jar ../META-INF/MANIFEST.MF *.class ../*.bmp ../*.png
---
+++ src/paletteEditor/ScreenVisualizer.java
@@ -0,0 +1,11 @@
+package paletteEditor;
+
+/**
+ * Allows to visualize palette changes
+ * Could be evolved into showing fonts changes later.
+ * @author eiyeron
+ *
+ */
+public class ScreenVisualizer {
+
+}
