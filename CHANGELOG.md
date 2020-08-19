a03f2d582c37a4a9815df6376c965142096e4a88
Johan Kotlinski <kotlinski@gmail.com>
2020-08-19 22:19:42 +0200

    1.4.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## [1.4.2] - 2020-08-19
+### Added
+
  - LSDj v8.8.3 support.
 
 ## [1.4.1] - 2020-07-04
--- MainWindow.java
+++ MainWindow.java
@@ -27,7 +27,7 @@ import javax.swing.border.*;
 import java.beans.*;
 
 public class MainWindow extends JFrame {
-    String versionString = "LSD-Patcher v1.4.1";
+    String versionString = "LSD-Patcher v1.4.2";
     JPanel contentPane;
     JPanel jPanel1 = new JPanel();
     TitledBorder titledBorder1;
