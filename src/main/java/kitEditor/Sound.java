3bb44f3702a31b02eede100af0258b1957b38b5c
Johan Kotlinski <kotlinski@gmail.com>
2020-10-11 23:34:56 +0200

    fix sample prelisten only playing back half sample

--- src/main/java/kitEditor/Sound.java
+++ src/main/java/kitEditor/Sound.java
@@ -10,25 +10,27 @@ public class Sound {
     private static final ArrayList<Clip> clipPool = new ArrayList<>();
 
     private static byte[] nibblesToWaveData(byte[] gbSample) {
-        byte[] waveData = new byte[gbSample.length * 2];
+        byte[] waveData = new byte[gbSample.length * 4];
         int src = 0;
         int dst = 0;
 
         while (src < gbSample.length) {
             byte sample = gbSample[src++];
+            waveData[dst++] = 0;
 
             // Emulates Game Boy sound chip bug. While changing waveform,
             // sound is played back at zero DC. This happens every 32'nd sample.
             if (dst % 32 == 0) {
                 waveData[dst] = 0x78;
             } else {
-                waveData[dst] = (byte) (0xf0 & sample);
+                waveData[dst] = (byte)(0xf0 & sample);
             }
             ++dst;
-            waveData[dst++] = (byte) ((0x0F & sample) << 4);
+            waveData[dst++] = 0;
+            waveData[dst++] = (byte)((0x0F & sample) << 4);
         }
-        for (int i = waveData.length / 2; i < waveData.length; ++i) {
-            waveData[i] = -128;
+        for (int i = 1; i < waveData.length; i += 2) {
+            waveData[i] += 128;
         }
         return waveData;
     }
@@ -55,7 +57,7 @@ public class Sound {
         final int sampleRate = halfSpeed ? 5734 : 11468;
         byte[] waveData = nibblesToWaveData(gbSample);
         Clip clip = getClip();
-        AudioFormat audioFormat = new AudioFormat(sampleRate, 8, 1, false, false);
+        AudioFormat audioFormat = new AudioFormat(sampleRate, 16, 1, false, false);
         clip.open(audioFormat, waveData, 0, waveData.length);
         FloatControl control = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         float result = 20f * (float) Math.log10(volume);
