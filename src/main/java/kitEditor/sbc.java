a5a536b46e2a0e6b608449c42eb227edd969e378
Johan Kotlinski <kotlinski@gmail.com>
2020-10-12 22:23:23 +0200

    removed dead/useless code

--- src/main/java/kitEditor/sbc.java
+++ src/main/java/kitEditor/sbc.java
@@ -14,23 +14,14 @@ class sbc {
 
             sample.seekStart();
             int sampleLength = sample.length();
-            // Trims the end of the sample to make it a multiple of 0x10.
-            sampleLength -= sampleLength % 0x10;
 
             int addedBytes = 0;
-
             int[] outputBuffer = new int[32];
             int outputCounter = 0;
             for (int i = 0; i < sampleLength; i++) {
                 int s = sample.readInt();
                 s -= Short.MIN_VALUE;
                 s /= 256 * 16;
-
-                /*
-                //this is to get middle at 7.5 instead of 8.0
-                outputBuffer[outputCounter]*=0xe;
-                outputBuffer[outputCounter]/=0xf;
-                */
 
                 //range check
                 s = Math.min(0xf, s);
